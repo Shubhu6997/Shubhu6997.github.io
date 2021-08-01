b46d3d2ddaadda18e79262d314547414d7648877
Shubhu6997 <shubhamhaval619@gmail.com>
2021-08-01 10:51:29 +0530

    Day 4 Assignments

---
+++ Day-3/Resume_In_JSON_Format/script.js
@@ -0,0 +1,40 @@
+var resume_data = {
+    Name : "Shubham Haval",
+    Designation : "Software Engineering Associates",
+    Current_Organization : "Amdocs",
+    Contact_Details : {
+        Mobile_No : "8805939067",
+        Email_ID : "shubhamhaval619@gmail.com",
+        LinkedIn_Profile : "https://www.linkedin.com/in/shubhamhaval-439ab6164",
+        Address : {
+            Address_Line1 : "G:2 Bhanudatta Apt",
+            Address_Line2 : "Karmveer Bhauroa Chowk",
+            City : "Miraj",
+            Pin_Code : "4164100",
+            District : "Sangli",
+            State : "Maharashtra",
+            Country : "India"
+        }
+    },
+    Professional_Experience : "Working as SRE to creating automations for customer issues and incident using Python, Shell Scripting, SQL and JSON.",
+    Projects : ["Created Flows to handle end to end incidents without manual intervention",
+    "To get start up logs of application servers via mail"],
+    Education : {
+        Degree : "BE",
+        College : "Sanjay Ghodawat Institute",
+        Stream : "CSE",
+        City : "Kolhapur",
+        Passing_Year : "2019"
+    },
+    Achivevements : ["Achieved award in Amdocs under team category 'We Are One Team Together We Play To Win'.",
+        "Achieved reward in Amdocs for good work in SRE Automation."],
+    Skills : {
+        Programming_Languages : ["Java", "Shell Scripting"],
+        Web_Technologies : ["HTML", "CSS", "JavaScript"],
+        Database : ["MySQL","Oracle"]
+    },
+    Strengths : ["Self Motivated", "Listening", "Responsible"],
+    Hobbies : ["Singing and Plying Guitar","Trekking and Bike Riding","Playing Cricket"]
+}
+
+console.log(resume_data);
