9ab4bfc787a7bea3bc83ce4fe3d7a538c87fb56f
Shubhu6997 <shubhamhaval619@gmail.com>
2021-08-20 09:51:01 +0530

    async

--- Promises with RestCountries API/Asia and Europe Countries/script.js
+++ Promises with RestCountries API/Asia and Europe Countries/script.js
@@ -1,3 +1,7 @@
+
+//Using Promise
+
+console.log("Using Promise");
 const AsiaRegion = fetch("https://restcountries.eu/rest/v2/region/asia")
     .then((data)=> data.json())
     .catch((errMgs)=> console.log(errMgs));
@@ -10,6 +14,20 @@ Promise.all([AsiaRegion, EuropeRegion])
 //.then((countries)=>((countries[0].concat(countries[1])).foreach((country)=>CountryFlag(country))));
 .then((countries)=>console.log(countries[0].concat(countries[1])))
 .catch((errMgs)=> console.log(errMgs));
+
+//Using async and await
+
+
+async function CountryData(){
+    const AsiaRegion = await fetch("https://restcountries.eu/rest/v2/region/asia");
+    const asiaData = await AsiaRegion.json();
+    const EuropeRegion = await fetch("https://restcountries.eu/rest/v2/region/europe");
+    const europeData = await EuropeRegion.json();
+    console.log("Using async and await");
+    console.log(asiaData.concat(europeData));
+}
+
+CountryData();
 
 /*
 function CountryFlag({name,flag,population,region,capital}){
--- Promises with RestCountries API/Counties in Europe who speaks Spanish/script.js
+++ Promises with RestCountries API/Counties in Europe who speaks Spanish/script.js
@@ -1,8 +1,20 @@
+//using Promise
+console.log("Using Promise")
 fetch("https://restcountries.eu/rest/v2/lang/es")
     .then((data)=> data.json())
     //.then((countries)=>countries.forEach((country)=>CountryFlag(country)))
     .then((countries)=>console.log(countries))
     .catch((errMgs)=> console.log(errMgs));
+
+//Using aync and await
+async function CountryData(){
+    const data = await fetch("https://restcountries.eu/rest/v2/lang/es");
+    const contries = await data.json();
+    console.log("Using async and await")
+    console.log(contries);
+}
+
+CountryData();
 
 /*
 function CountryFlag({name,flag,population,region,capital}){
