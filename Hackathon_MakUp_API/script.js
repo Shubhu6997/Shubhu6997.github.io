ddb3faf900f64cdf300cbb79da4bfcb5b9ed5a15
Shubhu6997 <shubhamhaval619@gmail.com>
2021-08-24 20:54:16 +0530

    MakeUp API V1

---
+++ Hackathon_MakUp_API/script.js
@@ -0,0 +1,43 @@
+const main_container = document.createElement("div");
+main_container.setAttribute("class", "main_container");
+document.body.append(main_container);
+
+/*const titles = document.createElement("div");
+titles.innerHTML = `<p>Confidence lies in the lipstick shade</p>`;
+main_container.append(titles);
+*/
+//const sub_container = document.createElement("div");
+//main_container.append(sub_container);
+
+
+
+async function getMakeUpAPIDetails() {
+
+    const data = await fetch("http://makeup-api.herokuapp.com/api/v1/products.json", {
+        method: "GET"
+    });
+
+    const apidata = await data.json();
+    apidata.forEach((product) => createProduct(product));
+}
+
+getMakeUpAPIDetails();
+
+function createProduct({ brand, name, price, price_sign, image_link, product_link, description }) {
+    const container = document.createElement("div");
+    container.setAttribute("class", "container");
+    container.innerHTML = `
+        <div class = "image_container">
+            <img src = "${image_link}" alt = "Product Image" onerror="this.onerror=null;this.src='img/default.png';">
+        </div>
+        <div class = "product_info">
+            <p class = "brand">${brand}</p>
+            <p class = "name">${name}</p>
+            <p class = "price">${price}${price_sign}</p>
+            <a href = "${product_link}" target = "_blank">Go To Product Page</a>
+           
+    
+        </div>`;
+    main_container.append(container);
+    // <p class = "description">${description}</p>
+}
